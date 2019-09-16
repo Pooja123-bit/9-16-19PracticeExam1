0e210e748d7105e0f65728ed92223e4f9904c74a
Pooja123-bit <poojaupadhyay100@gmail.com>
2019-09-16 18:03:54 -0500

    edit

---
+++ 9-16-19Practiceexam1.R
@@ -0,0 +1,101 @@
+m<-10
+m
+#Right to left
+
+10->k
+k
+#Left to right
+
+A<-"key"
+A
+typeof(A)
+#For Character data type
+
+x<-10:20
+typeof(x)
+
+or
+
+a<-10L
+typeof(a)
+#for integer data type
+
+y<-10
+class(y) 
+#for numeric data type
+
+f<-c(1,2,3,4,5,1,2)
+sf<-factor(f)
+sf
+#for factor data type
+
+20%%15
+20^15
+5^3
+20%/%15
+
+q<-1:10
+q
+
+a=1;b=2;c=3
+a;b;c
+#code for printing
+
+1*2+3
+1*(2+3)
+
+2^12345
+
+-2^1234
+
+0/0
+Inf/INf
+Inf/Inf
+
+a<-2
+2<-NULL
+
+Q<-c(1,2,3,4,5)
+length(Q)
+length(Q)<-7
+Q
+#NA
+
+'%Pooja%'<-function(u,v){2*u + 3*v}
+4%Pooja%5
+
+'%Neha%'<-function(u,v){15/u - 20/v}
+3%Neha%4
+
+r=rose
+h=hibiscus
+s=sunflower
+l=lavender
+switcheroo.if.then<-function(x){
+  if(x=="r")
+    "roses"
+  else if(x=="h")
+    "hibiscus"
+  else if(x=="s")
+    "sunflower"
+  else
+    "lavender"
+}
+switcheroo.if.then("r")
+switcheroo.if.then("h")
+switcheroo.if.then("s")
+switcheroo.if.then("t")
+
+load(file="fish_data.Rdata")
+str(fish)
+row(fish)
+col(fish)
+
+class(fish$year)
+typeof(fish$year)
+
+fish$area_fac<-as.character(fish$area_fac)
+str(fish)
+
+fish$avg.DNE.m<-NULL
+str(fish)
